8eaf6af25bd7e68078dd0deeb1d83cf4d2376f56
Grupo2 <gruponumero2sisinf2@outlook.com>
2022-04-12 23:22:50 -0400

    primer envio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { getInstructionStatements } from '@angular/compiler/src/render3/view/util';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ejemplo1';
+  nombre ='ivar';
+  apellido ='gutierrez';
+  texto='escriba su nombre aqui';
+  texto1="";
+  mario="https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg";
+  cambiartexto():void
+  {
+    this.texto="escriba su segundo nombre"
+  }
+
+  getsuma(n1: number,n2:number)
+{
+  return n1+n2;
 }
+}
+
